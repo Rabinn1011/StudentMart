f8ebd2f96728aff44732716003f19db974e1d993
Rabinn1011 <rabin.lc99@gmail.com>
2024-12-31 20:45:55 +0545

    don 2.0 + success/error msg update

--- StudX/store/views.py
+++ StudX/store/views.py
@@ -2,11 +2,12 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import PermissionDenied
+from django.views.defaults import permission_denied
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 
-from django.views.defaults import permission_denied
+
 
 from .forms import ProductForm,SignupForm, SellerForm
 from .models import Product, Seller_Details
@@ -43,8 +44,11 @@ def add_product(request):
                 return redirect('home')  # Redirect to a product list or another page
         else:
             form = ProductForm()
-        return render(request, 'add_product.html', {'form': form})
+        return render(request, template_name='add_product.html', context={'form': form})
+
     except PermissionDenied:
+
+        messages.error(request, "You need to log in or register to add a product.")
         return redirect('register')
 
 
@@ -62,13 +66,13 @@ def login_user(request):
             login(request, user)
             return redirect('home')
         else:
+            messages.error(request, "Invalid username or password.")
             return redirect('login')
     else:
-        return render(request, 'login.html',{})
+        return render(request, 'login.html', {})
 
 
 def register(request):
-
     form = SignupForm()
     if request.method == 'POST':
         form = SignupForm(request.POST)
@@ -79,16 +83,22 @@ def register(request):
             if User.objects.filter(email=email).exists():
                 messages.error(request, "This email is already registered. Please use a different email.")
                 return render(request, 'register.html', {'form': form})
-            form.save()  # Save User model
+
+            # Save User model
+            form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('login')
+
+            if user:
+                login(request, user)
+                messages.success(request, f"Welcome {username}! Your account has been successfully created.")
+                return redirect('login')  # Redirect to login or any desired page
+
         else:
-            return redirect('register')
-    else:
-        return render(request, 'register.html', {'form': form})
+            messages.error(request, "There was an error in your registration form. Please try again.")
+
+    return render(request, 'register.html', {'form': form})
 
 def logout_user(request):
     logout(request)
