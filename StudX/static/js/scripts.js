edd32a98f165115ccaa5fad49c21daf980468978
Rabinn1011 <rabin.lc99@gmail.com>
2025-01-08 20:32:47 +0545

    dropdown menu 3.0

--- StudX/static/js/scripts.js
+++ StudX/static/js/scripts.js
@@ -14,3 +14,21 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+//dropdown menu on clivk action
+
+const profileIcon = document.getElementById('profile-icon');
+const dropdownMenu = document.getElementById('dropdown-menu');
+
+// Toggle the dropdown menu on click
+profileIcon.addEventListener('click', (e) => {
+    e.preventDefault(); // Prevent default anchor behavior
+    e.stopPropagation(); // Prevent event from bubbling up
+    dropdownMenu.style.display =
+        dropdownMenu.style.display === 'block' ? 'none' : 'block';
+});
+
+// Hide the dropdown menu when clicking outside
+document.addEventListener('click', () => {
+    dropdownMenu.style.display = 'none';
+});
