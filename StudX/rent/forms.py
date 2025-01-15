48fa7b92b7258d57394bf74d56244cc2b9eb02f0
Prajwal Neupane <prajwalneupane4@gmail.com>
2025-01-15 23:59:19 +0545

    kei changes

--- StudX/rent/forms.py
+++ StudX/rent/forms.py
@@ -4,4 +4,4 @@ from .models import Room
 class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
-        fields=['owner_name','coordintate','owner_phone','photo','location','noOfRooms','moreDetails']
+        exclude = ['longitude','latitude', 'detailsBy']
