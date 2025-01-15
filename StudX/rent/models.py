301329dacd18dd8bc71c441d6d19aff04d302ca3
Prajwal Neupane <prajwalneupane4@gmail.com>
2025-01-15 23:59:19 +0545

    kei changes

--- StudX/rent/models.py
+++ StudX/rent/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Room(models.Model):
-    owner_name = models.CharField(max_length=100)
-    coordintate= models.CharField(max_length=100)
-    owner_phone = models.CharField(max_length=20)
-    photo = models.ImageField(upload_to='uploads/rooms/', null=True, blank=True)
-    type = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    noOfRooms = models.IntegerField()
-    detailsBy = models.ForeignKey(User, on_delete=models.CASCADE)
-    moreDetails = models.TextField()
+        owner_name = models.CharField(max_length=100)
+        latitude = models.FloatField()
+        longitude = models.FloatField()
+        owner_phone = models.CharField(max_length=20)
+        photo = models.ImageField(upload_to='uploads/rooms/', null=True, blank=True)
+        type = models.CharField(max_length=100)
+        noOfRooms = models.IntegerField()
+        detailsBy = models.ForeignKey(User, on_delete=models.CASCADE)
+        moreDetails = models.TextField()
 
-    def __str__(self):
-        return self.owner_name
+        def __str__(self):
+            return self.owner_name
--- StudX/rent/views.py
+++ StudX/rent/views.py
@@ -1,21 +1,37 @@
+from django.http import Http404
 from django.shortcuts import render, redirect
+from pyexpat.errors import messages
 
 from rent.forms import RoomForm
 from rent.models import Room
 
 
-# Create your views here.
 def room(request):
     rooms = Room.objects.all()
 
 def add_Room(request):
     if request.method == 'POST':
         form = RoomForm(request.POST, request.FILES)  # Handle form data and uploaded files
+        latitude =request.POST.get('latitude')
+        longitude =request.POST.get('longitude')
         if form.is_valid():
             room = form.save(commit=False)
             room.detailsBy = request.user  # Set the current user as the seller
+            room.latitude = latitude
+            room.longitude = longitude
             room.save()
+            messages.success(request, 'Room Added')
             return redirect('home')  # Redirect to home or another page
+        messages.error(request, 'Room Not Added')
     else:
         form = RoomForm()
     return render(request, 'add_room.html', {'form': form})
+
+def room_detail(request, room_id):
+    try:
+        room = Room.objects.get(id=room_id)
+
+    except Room.DoesNotExist:
+        raise Http404("Room not found")
+
+    return render(request, 'roomdetails.html', {'room': room})
