4e3ebcf7e384a1dbcebcb4f655c48bd2859a9b8b
Prajwal Neupane <prajwalneupane4@gmail.com>
2025-01-25 00:36:11 +0545

    doing Cart

--- StudX/StudX/urls.py
+++ StudX/StudX/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls')),
     path('',include('rent.urls')),
+    path('',include('cart.urls')),
 
 ]+ static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT)
