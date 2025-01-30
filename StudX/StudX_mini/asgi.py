aef8532e8f4927a55a07a825d4c5f1250e23d3a0
Rabinn1011 <rabin.lc99@gmail.com>
2025-01-30 09:19:15 +0545

    mero ma naam change

--- StudX/StudX_mini/asgi.py
+++ StudX/StudX_mini/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for StudX project.
+ASGI config for StudX_mini project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StudX.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StudX_mini.settings')
 
 application = get_asgi_application()
